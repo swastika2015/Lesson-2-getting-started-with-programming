eb06eed059a2cb5a9a0467d3bfbc154f65c5f324
swastika2015 <jmswastika18409@gmail.com>
2026-05-02 13:22:17 +0530

    Add files via upload

---
+++ print_commands.py
@@ -0,0 +1,10 @@
+#print a message
+print("Welcome to programming world")
+#print a number
+print(246)
+#print multiple arguements
+print("hello",6547)
+#print next line
+print("hello python  ") 
+#end argument
+print("bye",end= "*")
