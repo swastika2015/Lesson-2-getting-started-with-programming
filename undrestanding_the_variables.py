0ebc8fa31dc52288c4a23ebbb6ad340a2a8030d4
swastika2015 <jmswastika18409@gmail.com>
2026-05-02 13:33:13 +0530

    Add files via upload

---
+++ undrestanding_the_variables.py
@@ -0,0 +1,14 @@
+#printing arguements
+
+x="Hello"
+y=245857531
+print(x)
+print(y)
+
+Codingal="I love coding"
+print(Codingal)
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+
+print("\nHello",name,"\nWelcome to Python")
